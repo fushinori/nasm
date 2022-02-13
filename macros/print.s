7eec5b924a87c8c2644e10927aba74c758f561d4
Rishi <fushinoriii@gmail.com>
2022-02-13 18:33:50 +0530

    Macros

---
+++ macros/print.s
@@ -0,0 +1,13 @@
+%include "macros.mac"
+
+global _start
+
+section .data
+msg1 db "Hello there!", 0ah, 0  ; end with 0, the null byte
+msg2 db "Goodbye!", 0ah, 0
+
+section .text
+_start:
+    print msg1
+    print msg2
+    exit 0
